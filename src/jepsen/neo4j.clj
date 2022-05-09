17c9a351327644c82ac5f212df9a8e88cc41fddc
Anastasia-98Moiseeva <nepro100nastya@yandex.ru>
2022-05-09 16:55:49 +0300

    start neo4j

--- src/jepsen/neo4j.clj
+++ src/jepsen/neo4j.clj
@@ -9,6 +9,33 @@
             [jepsen.os.debian :as debian]))
             
 (def dir     "/opt/neo4j")
+(def binary  (str dir "/bin/neo4j"))
+(def logfile (str dir "/neo4j.log"))
+(def pidfile (str dir "/neo4j.pid"))
+
+(defn node-url
+  "An HTTP url for connecting to a node on a particular port."
+  [node port]
+  (str "http://" node ":" port))
+
+(defn peer-url
+  "The HTTP url for other peers to talk to a node."
+  [node]
+  (node-url node 2380))
+
+(defn client-url
+  "The HTTP url clients use to talk to a node."
+  [node]
+  (node-url node 2379))
+
+(defn initial-cluster
+  "Constructs an initial cluster string for a test, like
+  \"foo=foo:2380,bar=bar:2380,...\""
+  [test]
+  (->> (:nodes test)
+       (map (fn [node]
+              (str node "=" (peer-url node))))
+       (str/join ",")))
             
 (defn db
   "Neo4i DB for a particular version."
@@ -18,10 +45,33 @@
       (info node "installing neo4j" version)
       (c/su
         (let [url (str "https://dl.dropboxusercontent.com/s/jnw606op0lzk3hq/neo4j-community-" version "-unix.tar.gz")]
-          (cu/install-archive! url dir))))
-        
+          (cu/install-archive! url dir))
+
+        (cu/start-daemon!
+          {:logfile logfile
+           :pidfile pidfile
+           :chdir   dir}
+          binary
+          :--log-output :stderr
+          :--name (name node)
+          :--listen-peer-urls (peer-url node)
+          :--listen-client-urls (client-url node)
+          :--advertise-client-urls (client-url node)
+          :--initial-cluster-state :new
+          :--initial-advertise-peer-urls (peer-url node)
+          :--initial-cluster (initial-cluster test))
+
+        (Thread/sleep 10000)))
+
     (teardown! [_ test node]
-      (info node "tearing down neo4j"))))
+      (info node "tearing down neo4j")
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir))
+      )
+
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
          
 (defn neo4j-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
