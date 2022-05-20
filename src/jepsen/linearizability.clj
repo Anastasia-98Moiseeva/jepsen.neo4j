86ead1402c46cc9242c090d99c2419c9ed43391a
Anastasia-98Moiseeva <nepro100nastya@yandex.ru>
2022-05-21 00:19:37 +0300

    add workload

---
+++ src/jepsen/linearizability.clj
@@ -0,0 +1,51 @@
+(ns jepsen.linearizability
+  (:require [neo4clj.client :as nc]
+            [jepsen [client :as client]
+             [checker :as checker]
+             [generator :as gen]]
+            [knossos.model :as model]
+            [clojure.tools.logging :refer :all]
+            [jepsen.checker.timeline :as timeline]))
+
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 10)})
+
+(defrecord Client [conn]
+  client/Client
+  (open! [this test node]
+    (assoc this :conn (nc/connect (str "neo4j://192.168.0.105:7687") "neo4j" "pas")))
+
+  (setup! [this test]
+    (info (nc/create-node! conn {:ref-id "p"
+                                 :labels [:person]
+                                 :props  {:name "Alice"}})))
+
+  (invoke! [_ test op]
+    (case (:f op)
+      :read (assoc op
+              :type :ok,
+              :value (get-in (nc/find-node conn {:ref-id "n"
+                                                 :props  {:name "Alice"}}) [:props :rating]))
+      :write (do
+               (nc/update-props! conn {:props {:name "Alice"}} {:rating (:value op)})
+               (assoc op :type :ok))
+      )
+    )
+
+  (teardown! [this test])
+
+  (close! [_ test]
+    (nc/disconnect conn)))
+
+
+(defn workload
+  "Stuff you need to build a test!"
+  [opts]
+  {:client    (Client. nil)
+   :generator (->> (gen/mix [r w])
+                   (gen/stagger 1/50)
+                   (gen/nemesis nil)
+                   (gen/time-limit (:time-limit opts)))
+   :checker   (checker/linearizable
+                {:model     (model/register)
+                 :algorithm :linear})})
--- src/jepsen/neo4j.clj
+++ src/jepsen/neo4j.clj
@@ -9,9 +9,9 @@
             [jepsen.control.util :as cu]
             [jepsen.os.debian :as debian]
             [knossos.model :as model]
-            [jepsen.neoclient]
+            [jepsen.linearizability :as lin]
             [jepsen.checker.timeline :as timeline])
-  (:import (jepsen.neoclient Client)))
+  (:import (jepsen.linearizability Client)))
 
 (def dir "/opt/neo4j")
 (def binary (str dir "/bin/neo4j"))
@@ -45,32 +45,41 @@
     (log-files [_ test node]
       [logfile])))
 
+(def workloads
+  "A map of workload names to functions that can take opts and construct
+  workloads."
+  {:linear lin/workload})
+
+(def cli-opts
+  "Additional command line options."
+  [["-w" "--workload NAME" "Test workload to run"
+    :default :linear
+    :parse-fn keyword
+    :validate [workloads (cli/one-of workloads)]]])
 
 (defn neo4j-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency ...), constructs a test map."
   [opts]
-  (merge tests/noop-test
-         opts
-         {:name            "neo4j"
-          :os              debian/os
-          :db              (db "4.2.16")
-          :pure-generators true
-          :client          (Client. nil)
-          :checker         (checker/compose
-                             {:linear   (checker/linearizable
-                                          {:model     (model/register)
-                                           :algorithm :linear})
-                              :timeline (timeline/html)})
-          :generator       (->> (gen/mix [r w])
-                                (gen/stagger 1/50)
-                                (gen/nemesis nil)
-                                (gen/time-limit (:time-limit opts)))}))
+  (let [workload ((get workloads (:workload opts)) opts)
+        gen (->> (:generator workload))]
+    (merge tests/noop-test
+           opts
+           {:name            "neo4j"
+            :os              debian/os
+            :db              (db "4.2.16")
+            :pure-generators true
+            :client          (:client workload)
+            :checker         (checker/compose
+                               {:workload (:checker workload)
+                                :timeline (timeline/html)})
+            :generator       gen})))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (merge (cli/single-test-cmd {:test-fn neo4j-test})
+  (cli/run! (merge (cli/single-test-cmd {:test-fn  neo4j-test
+                                         :opt-spec cli-opts})
                    (cli/serve-cmd))
             args))
